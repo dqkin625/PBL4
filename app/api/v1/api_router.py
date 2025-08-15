a241bbce929425bd57e40b83c994ae71d4742b09
dqkin625 <dqkin625@gmail.com>
2025-08-16 01:37:53 +0700

    cryptonews_api

--- app/api/v1/api_router.py
+++ app/api/v1/api_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import coindesk
+from app.api.v1.endpoints import coindesk, cryptonews
 
 router = APIRouter()
 
 router.include_router(coindesk.router, prefix="/v1", tags=["coindesk_news"])
+router.include_router(cryptonews.router, prefix="/v1", tags=["cryptonews_news"])
---
+++ app/api/v1/endpoints/cryptonews.py
@@ -0,0 +1,35 @@
+import feedparser
+from bs4 import BeautifulSoup
+from fastapi import APIRouter
+
+router = APIRouter()
+
+CRYPTONEWS_RSS_FEED_URL = "https://cryptonews.com/news/feed/"
+
+@router.get("/cryptonews_news")
+def get_news():
+    feed = feedparser.parse(CRYPTONEWS_RSS_FEED_URL)
+    news_items = []
+    
+    for entry in feed.entries:
+
+        from pprint import pprint
+        print("\n==== ENTRY ====")
+        pprint(entry)
+
+        content_html = entry.get("content", [{}])[0].get("value", "")
+        content_text = BeautifulSoup(content_html, "html.parser").getText()
+        
+        news_items.append(
+            {
+                "title": entry.get("title", ""),
+                "link": entry.get("link", ""),
+                "media": entry.get("media_content", [{}])[0].get("url",""),
+                "published": entry.get("published",""),
+                # "description": entry.get("title_detail", {}).get("value",""),
+                "author": entry.get("authors", ""),
+                "content": content_text
+            }
+        )
+    
+    return {"crytonews_news": news_items}
