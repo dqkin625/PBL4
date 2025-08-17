e9999add3a3c97892e68f2c27caee93b84f203b3
dqkin625 <dqkin625@gmail.com>
2025-08-18 00:32:10 +0700

    Fix UTC+7

--- app/api/v1/endpoints/coindesk.py
+++ app/api/v1/endpoints/coindesk.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
 import feedparser
 from bs4 import BeautifulSoup
 from fastapi import APIRouter
@@ -20,12 +22,15 @@ def get_news():
         content_html = entry.get("content", [{}])[0].get("value", "")
         content_text = BeautifulSoup(content_html, "html.parser").getText()
         
+        dt_str = entry.get("published","")
+        dt = datetime.strptime(dt_str, "%a, %d %b %Y %H:%M:%S %z")
+
         news_items.append(
             {
                 "url": entry.get("link", ""),
                 "title": entry.get("title", ""),
                 "media": entry.get("media_content", [{}])[0].get("url",""),
-                "published_time": entry.get("published",""),
+                "published_time": dt.astimezone(ZoneInfo("Asia/Ho_Chi_Minh")),
                 # "description": entry.get("title_detail", {}).get("value",""),
                 "author": entry.get("authors", ""),
                 "content": content_text
